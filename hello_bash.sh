6dfee09c536980eb29e7143db2b2b4fd1cf9e02c
Omar West <omarw@ascend-rw01.ten.osc.edu>
2025-07-08 11:57:17 -0400

    Add shell scripts and data files

---
+++ hello_bash.sh
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "9ef84f29-5f7a-4ff8-97a1-55eb1690ac83",
+   "metadata": {},
+   "source": [
+    "# Hello World in Bash\n",
+    "This notebook demonstrates a Bash script using Jupyter's `%%bash` magic."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "f5a59a33-adf8-49dd-b32b-7afcb52777c6",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello, World from Bash!\n"
+     ]
+    }
+   ],
+   "source": [
+    "%%bash\n",
+    "echo \"Hello, World from Bash!\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python (7030_class_1)",
+   "language": "python",
+   "name": "7030_class_1"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.18"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
